55967edece9f44abc4ae135a5c7264745ee25cc3
HomescuLiviu <lhomescu@gmail.com>
2016-05-15 15:36:33 +0300

    entitystore methods

--- src/main/java/com/omegadeveloper/entity/database/EntityServiceImpl.java
+++ src/main/java/com/omegadeveloper/entity/database/EntityServiceImpl.java
@@ -1,7 +1,18 @@
 package com.omegadeveloper.entity.database;
 
 
+import javax.swing.text.html.parser.Entity;
+
 public class EntityServiceImpl implements EntityService {
 
 
+    @Override
+    public void storeEntity(Entity entity) {
+
+    }
+
+    @Override
+    public Entity getEntity(String id) {
+        return null;
+    }
 }
---
+++ src/test/java/com/omegadeveloper/entity/database/EntityServiceImplTest.java
@@ -0,0 +1,19 @@
+package com.omegadeveloper.entity.database;
+
+import org.junit.Test;
+
+/**
+ * Created by liviu on 5/15/2016.
+ */
+public class EntityServiceImplTest {
+
+    @Test
+    public void testStoreEntity() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEntity() throws Exception {
+
+    }
+}
